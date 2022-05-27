6a36723af881f5576bc4a0e947bf022a55c25149
Sean Mackin <seanamackin15@gmail.com>
2022-05-27 07:03:32 -0400

    added styling and second section

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ document.addEventListener("DOMContentLoaded", main);
 function revealMenu(evt) {
   const contentBox = document.querySelector(".content");
   const navMenu = document.querySelector(".nav-links");
-  const menuSize = window.getComputedStyle(navMenu).getPropertyValue("width")
-
-  console.log(menuSize);
+  // size of elements in px measurements
+  const menuSize = parseInt(window.getComputedStyle(navMenu).getPropertyValue("width"));
+  const totalWidth = parseInt(window.innerWidth);
+  // all this work is to make sure that changes to the css rule track as percentages so changes in window size dont
+  // fuck up the rendering of the page, unfortunately getting access to that is difficult so we compute it from the
+  // copmuted sizes and round to the nearest integer (as we wont use non-integer inputs in the css for the nav links width)
   if (contentBox.style.left == 0 || contentBox.style.left == "0px") {
-    contentBox.style.left = menuSize;
-    contentBox.style.width = (parseInt(window.innerWidth)-parseInt(menuSize)) + "px";
-  } else if (contentBox.style.left == menuSize){
+    contentBox.style.left = Math.round(100 * menuSize / totalWidth) + "%" ;
+    contentBox.style.width = Math.round(100 * (totalWidth-menuSize)/totalWidth)+ "%";
+  } else if (contentBox.style.left == Math.round(100 * menuSize / totalWidth) + "%"){
     contentBox.style.left = "0";
     contentBox.style.width = "100%";
   }
